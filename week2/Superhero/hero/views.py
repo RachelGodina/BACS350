83852bdbaae98d513dae2f1c13f7d5e8efb0816c
RachelGodina <89423392+RachelGodina@users.noreply.github.com>
2021-09-03 22:06:01 -0600

    build superhero project

--- week2/Superhero/hero/views.py
+++ week2/Superhero/hero/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from os import terminal_size
+from django.views.generic import TemplateView
 
-# Create your views here.
+class wonderWomanView(TemplateView):
+    template_name = 'wonderWoman.html'
+    
+    def get_context_data(self, **kwargs):
+        return {
+            'title': 'About this Class', 
+            'body': 'Once upon a time ...',
+        }
+ 
+class blackWidowView(TemplateView):
+    template_name = "blackWidow.html"
+
+class gamoraView(TemplateView):
+    template_name = 'gamora.html'
