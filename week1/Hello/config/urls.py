8e33e9faeff5a3028c28b40bcbd3302f755c4fec
RachelGodina <89423392+RachelGodina@users.noreply.github.com>
2021-09-03 22:05:39 -0600

    built superhero project

--- week1/Hello/config/urls.py
+++ week1/Hello/config/urls.py
@@ -15,13 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from pages.views import homePageView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
-
-from django.urls import path
-from pages.views import homePageView
 
 urlpatterns = [
     path('', homePageView)
