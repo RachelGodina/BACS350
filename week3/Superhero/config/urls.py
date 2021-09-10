9b342e9545acd9ea610b3f3920d69c02568efb6f
RachelGodina <89423392+RachelGodina@users.noreply.github.com>
2021-09-10 15:03:11 -0600

    basic page

--- week3/Superhero/config/urls.py
+++ week3/Superhero/config/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from hero.views import blackWidowView, wonderWomanView, gamoraView
+from hero.views import blackWidowView, wonderWomanView, gamoraView, IndexView
 
 urlpatterns = [
+    path('', IndexView.as_view()),
     path('blackWidow', blackWidowView.as_view()),
     path('wonderWoman', wonderWomanView.as_view()),
     path('gamora', gamoraView.as_view()),
--- week3/Superhero/hero/views.py
+++ week3/Superhero/hero/views.py
@@ -1,6 +1,14 @@
 from os import terminal_size
 from django.views.generic import TemplateView
 
+class IndexView(TemplateView):
+    template_name = 'index.html'
+    
+    # def get_context_data(self, **kwargs):
+    #     return {
+    #         'title': 'About this Class', 
+    #         'body': 'Once upon a time ...',
+    #     }
 class wonderWomanView(TemplateView):
     template_name = 'wonderWoman.html'
     
