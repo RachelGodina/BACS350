6967722ce931aec2138dbdee95b1c6ad3612a45e
RachelGodina <89423392+RachelGodina@users.noreply.github.com>
2021-10-05 15:34:15 -0600

    git

---
+++ week6/Superhero/hero/views.py
@@ -0,0 +1,35 @@
+from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import TemplateView
+from django.views.generic import ListView, CreateView, DeleteView, DetailView, RedirectView, UpdateView
+from .models import Superhero
+from .models import Hero
+
+class superheroView(RedirectView):
+    url = '/superhero/'
+
+class IndexView(TemplateView):
+    template_name = 'index.html'
+
+class SuperHeroListView(ListView):
+    model = Superhero
+    template_name = 'superhero_list.html'
+
+class SuperHeroDetailView(DetailView):
+    model = Superhero
+    template_name = 'superhero_detail.html'
+
+class SuperheroCreateView(CreateView):
+    model = Superhero
+    template_name = 'superhero_new.html'
+    fields = ['name', 'identity', 'description', 'strength', 'weakness', 'image']
+
+class SuperheroUpdateView(UpdateView):
+    model = Superhero
+    template_name = 'superhero_edit.html'
+    fields = ['name', 'identity', 'description', 'strength', 'weakness', 'image']
+
+class SuperheroDeleteView(DeleteView):
+    model = Superhero
+    template_name = 'superhero_delete.html'
+    success_url = reverse_lazy('superhero_list')
